b20d2395e1d1574fec7436a73479d31b4eb87eee
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-01 20:39:28 -0300

    add path aliases

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,6 +1,6 @@
-import { Entity } from "../../core/entities/entity";
-import { Optional } from "../../core/types/optional";
-import { UniqueEntityID } from "../../core/entities/unique-entity-id";
+import { Entity } from "@/core/entities/entity";
+import { Optional } from "@/core/types/optional";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 
 interface AnswerProps {
   authorId: UniqueEntityID;
