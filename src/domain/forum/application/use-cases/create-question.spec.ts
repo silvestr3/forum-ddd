2bd7040a772956d7e10f236b271c5f08a29ccf7c
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-02 13:58:00 -0300

    refactor tests

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -1,21 +1,24 @@
-import { Question } from "../../enterprise/entities/question";
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
 import { QuestionsRepository } from "../repositories/questions-repository";
 import { CreateQuestionUseCase } from "./create-question";
 
-const fakeQuestionsRepository: QuestionsRepository = {
-  create: async (question: Question) => {
-    return;
-  },
-};
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let sut: CreateQuestionUseCase;
 
-test("create a question", async () => {
-  const createQuestion = new CreateQuestionUseCase(fakeQuestionsRepository);
-
-  const { question } = await createQuestion.execute({
-    authorId: "1",
-    content: "Question content",
-    title: "New question",
+describe("Create question use case tests", () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    sut = new CreateQuestionUseCase(inMemoryQuestionsRepository);
   });
 
-  expect(question.id).toBeTruthy();
+  it("should be able to create a question", async () => {
+    const { question } = await sut.execute({
+      authorId: "1",
+      content: "Question content",
+      title: "New question",
+    });
+
+    expect(question.id).toBeTruthy();
+    expect(inMemoryQuestionsRepository.items[0].id).toEqual(question.id);
+  });
 });
