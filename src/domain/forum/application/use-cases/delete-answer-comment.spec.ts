b2b9356df7e1e87ecd5a0370ac8dbf048877219b
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-04 18:44:23 -0300

    delete answer comment use case

---
+++ src/domain/forum/application/use-cases/delete-answer-comment.spec.ts
@@ -0,0 +1,40 @@
+import { InMemoryAnswerCommentsRepository } from "test/repositories/in-memory-answer-comments-repository";
+import { DeleteAnswerCommentUseCase } from "./delete-answer-comment";
+import { MakeAnswerComment } from "test/factories/make-answer-comment";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository;
+let sut: DeleteAnswerCommentUseCase;
+
+describe("Delete answer comment tests", () => {
+  beforeEach(() => {
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository();
+    sut = new DeleteAnswerCommentUseCase(inMemoryAnswerCommentsRepository);
+  });
+
+  it("should be able to delete a answer comment", async () => {
+    const answerComment = MakeAnswerComment();
+    await inMemoryAnswerCommentsRepository.create(answerComment);
+
+    await sut.execute({
+      answerCommentId: answerComment.id.toString(),
+      authorId: answerComment.authorId.toString(),
+    });
+
+    expect(inMemoryAnswerCommentsRepository.items).toHaveLength(0);
+  });
+
+  it("should not be able to delete another users answer comment", async () => {
+    const answerComment = MakeAnswerComment({
+      authorId: new UniqueEntityID("author-1"),
+    });
+    await inMemoryAnswerCommentsRepository.create(answerComment);
+
+    await expect(() =>
+      sut.execute({
+        answerCommentId: answerComment.id.toString(),
+        authorId: "author-2",
+      })
+    ).rejects.toBeInstanceOf(Error);
+  });
+});
--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -9,4 +9,18 @@ export class InMemoryAnswerCommentsRepository
   async create(answerComment: AnswerComment) {
     this.items.push(answerComment);
   }
+
+  async delete(answerComment: AnswerComment) {
+    const indexId = this.items.findIndex(
+      (item) => item.id === answerComment.id
+    );
+
+    this.items.splice(indexId, 1);
+  }
+
+  async findById(id: string) {
+    const answerComment = this.items.find((item) => item.id.toString() === id);
+
+    return answerComment ?? null;
+  }
 }
