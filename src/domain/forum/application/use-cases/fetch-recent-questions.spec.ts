50a52339d10b2280a2c5c5a7b922c038e6b57e6b
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-04 16:35:25 -0300

    fetch recent questions use case

---
+++ src/domain/forum/application/use-cases/fetch-recent-questions.spec.ts
@@ -0,0 +1,54 @@
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
+import { GetQuestionBySlugUseCase } from "./get-question-by-slug";
+import { MakeQuestion } from "test/factories/make-question";
+import { FetchRecentQuestionsUseCase } from "./fetch-recent-questions";
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let sut: FetchRecentQuestionsUseCase;
+
+describe("Fetch recent questions", () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    sut = new FetchRecentQuestionsUseCase(inMemoryQuestionsRepository);
+  });
+
+  it("should be able to fetch recent questions", async () => {
+    await inMemoryQuestionsRepository.create(
+      MakeQuestion({ createdAt: new Date(2022, 0, 20) })
+    );
+    await inMemoryQuestionsRepository.create(
+      MakeQuestion({ createdAt: new Date(2022, 0, 18) })
+    );
+    await inMemoryQuestionsRepository.create(
+      MakeQuestion({ createdAt: new Date(2022, 0, 23) })
+    );
+
+    const { questions } = await sut.execute({
+      page: 1,
+    });
+
+    expect(questions).toEqual([
+      expect.objectContaining({
+        createdAt: new Date(2022, 0, 23),
+      }),
+      expect.objectContaining({
+        createdAt: new Date(2022, 0, 20),
+      }),
+      expect.objectContaining({
+        createdAt: new Date(2022, 0, 18),
+      }),
+    ]);
+  });
+
+  it("Should be able to fetch paginated recent questions", async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryQuestionsRepository.create(MakeQuestion());
+    }
+
+    const { questions } = await sut.execute({
+      page: 2,
+    });
+
+    expect(questions).toHaveLength(2);
+  });
+});
--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { QuestionsRepository } from "@/domain/forum/application/repositories/questions-repository";
 import { Question } from "@/domain/forum/enterprise/entities/question";
 import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
@@ -31,5 +32,13 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
     const index = this.items.findIndex((item) => item.id === question.id);
 
     this.items[index] = question;
+  }
+
+  async findManyRecent({ page }: PaginationParams) {
+    const questions = this.items
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20);
+
+    return questions;
   }
 }
