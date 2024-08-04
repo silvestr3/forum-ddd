10c42eff6cbcc6d6d6af1fafe6d091e5b7194923
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-04 17:34:36 -0300

    comment on answer use case

---
+++ src/domain/forum/application/use-cases/comment-on-answer.spec.ts
@@ -0,0 +1,34 @@
+import { InMemoryAnswersRepository } from "test/repositories/in-memory-answers-repository";
+import { MakeAnswer } from "test/factories/make-answer";
+import { InMemoryAnswerCommentsRepository } from "test/repositories/in-memory-answer-comments-repository";
+import { CommentOnAnswerUseCase } from "./comment-on-answer";
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository;
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository;
+let sut: CommentOnAnswerUseCase;
+
+describe("Comment on answer tests", () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository();
+    inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository();
+    sut = new CommentOnAnswerUseCase(
+      inMemoryAnswersRepository,
+      inMemoryAnswerCommentsRepository
+    );
+  });
+
+  it("should be able to comment on answer", async () => {
+    const answer = MakeAnswer();
+    await inMemoryAnswersRepository.create(answer);
+
+    await sut.execute({
+      answerId: answer.id.toString(),
+      authorId: "author-1",
+      content: "This is a test comment",
+    });
+
+    expect(inMemoryAnswerCommentsRepository.items[0].content).toEqual(
+      "This is a test comment"
+    );
+  });
+});
