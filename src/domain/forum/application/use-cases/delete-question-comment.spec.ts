a68e268001fe4185f18433de21d8b9c2e08f377e
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-04 17:48:59 -0300

    delete question comment use case

---
+++ src/domain/forum/application/use-cases/delete-question-comment.spec.ts
@@ -0,0 +1,44 @@
+import { InMemoryQuestionsRepository } from "test/repositories/in-memory-questions-repository";
+import { MakeQuestion } from "test/factories/make-question";
+import { InMemoryQuestioCommentsRepository } from "test/repositories/in-memory-question-comments-repository";
+import { CommentOnQuestionUseCase } from "./comment-on-question";
+import { DeleteQuestionCommentUseCase } from "./delete-question-comment";
+import { MakeQuestionComment } from "test/factories/make-question-comment";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+
+let inMemoryQuestionCommentsRepository: InMemoryQuestioCommentsRepository;
+let sut: DeleteQuestionCommentUseCase;
+
+describe("Delete question comment tests", () => {
+  beforeEach(() => {
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestioCommentsRepository();
+    sut = new DeleteQuestionCommentUseCase(inMemoryQuestionCommentsRepository);
+  });
+
+  it("should be able to delete a question comment", async () => {
+    const questionComment = MakeQuestionComment();
+    await inMemoryQuestionCommentsRepository.create(questionComment);
+
+    await sut.execute({
+      questionCommentId: questionComment.id.toString(),
+      authorId: questionComment.authorId.toString(),
+    });
+
+    expect(inMemoryQuestionCommentsRepository.items).toHaveLength(0);
+  });
+
+  it("should not be able to delete another users question comment", async () => {
+    const questionComment = MakeQuestionComment({
+      authorId: new UniqueEntityID("author-1"),
+    });
+    await inMemoryQuestionCommentsRepository.create(questionComment);
+
+    await expect(() =>
+      sut.execute({
+        questionCommentId: questionComment.id.toString(),
+        authorId: "author-2",
+      })
+    ).rejects.toBeInstanceOf(Error);
+  });
+});
--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -9,4 +9,20 @@ export class InMemoryQuestioCommentsRepository
   async create(questionComment: QuestionComment) {
     this.items.push(questionComment);
   }
+
+  async delete(questionComment: QuestionComment) {
+    const indexId = this.items.findIndex(
+      (item) => item.id === questionComment.id
+    );
+
+    this.items.splice(indexId, 1);
+  }
+
+  async findById(id: string) {
+    const questionComment = this.items.find(
+      (item) => item.id.toString() === id
+    );
+
+    return questionComment ?? null;
+  }
 }
