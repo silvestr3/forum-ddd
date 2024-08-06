bb397ee099e73d28b4f30f0bb340d976bf06c294
silvestr3 <fellipe.silvestre@outlook.com>
2024-08-06 18:41:13 -0300

    implement dispatch event for create new answer notification

--- test/repositories/in-memory-answers-repository.ts
+++ test/repositories/in-memory-answers-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from "@/core/events/domain-events";
 import { PaginationParams } from "@/core/repositories/pagination-params";
 import { AnswerAttachmentsRepository } from "@/domain/forum/application/repositories/answer-attachments-repository";
 import { AnswersRepository } from "@/domain/forum/application/repositories/answers-repository";
@@ -12,6 +13,8 @@ export class InMemoryAnswersRepository implements AnswersRepository {
 
   async create(answer: Answer): Promise<void> {
     this.items.push(answer);
+
+    DomainEvents.dispatchEventsForAggregate(answer.id);
   }
 
   async delete(answer: Answer) {
@@ -32,6 +35,7 @@ export class InMemoryAnswersRepository implements AnswersRepository {
     const index = this.items.findIndex((item) => item.id === answer.id);
 
     this.items[index] = answer;
+    DomainEvents.dispatchEventsForAggregate(answer.id);
   }
 
   async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
